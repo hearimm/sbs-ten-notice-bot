4dd23ce48b52dd0eb8975518c5c1bf58e8ce08fa
Hulk Choi <doksul23@gmail.com>
2018-05-11 19:50:47 +0900

    생녹방 여부 추가

--- main_task.rb
+++ main_task.rb
@@ -8,7 +8,7 @@ task = Repository::Task.instance
 result_task = task.find_one
 
 if !result_task.nil? && result_task['time'] < Time.now + (60 * 10)
-  Sender.send_message(result_task['desc'])
+  Sender.send_message(result_task['desc'] + " [생녹방]")
   p task.delete_one(result_task['_id'])
 else
   p "nothing"
